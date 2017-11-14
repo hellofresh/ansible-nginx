fcb0157646cfde3617a740ec88e3e359cbcd2f18
Ali Rizwan <aleerizw@gmail.com>
2017-11-14 12:29:18 +0100

    Generalized fragment templates to location templates. (#5)
    * Generalized fragment templates to location templates.
    
    * Skip task if variable is not defined.
    
    * Fixed template overwriting common_config

--- test/integration/simple/serverspec/nginx_spec.rb
+++ test/integration/simple/serverspec/nginx_spec.rb
@@ -33,34 +33,51 @@ describe 'NGinx sites-enabled' do
 
 end
 
-describe 'Nginx fragments' do
-  describe file('/etc/nginx/fragments/') do
+describe 'Nginx locations' do
+  describe file('/etc/nginx/include.d/') do
     it { should be_directory }
   end
-  describe file('/etc/nginx/fragments/fragment.upstreams.conf') do
-    it { should exist }
-    it { should contain 'upstream some-upstream {'}
-    it { should contain 'server localhost'}
-  end
-  describe file('/etc/nginx/fragments/fragments.conf') do
+  describe file('/etc/nginx/include.d/fragment_locations.conf') do
     it { should exist }
     it { should contain 'location ~* ^/fragment1($|/.*$) {'}
     it { should contain 'proxy_pass http://some-upstream;'}
     it { should contain 'proxy_set_header X-Forwarded-Proto https'}
     it { should contain 'proxy_set_header Proxy \'\''}
   end
-  describe file('/etc/nginx/fragments/fragments.conf') do
+  describe file('/etc/nginx/include.d/fragment_locations.conf') do
     it { should exist }
     it { should contain 'location ~* ^/fragment2($|/.*$) {'}
     it { should contain 'proxy_pass http://some-upstream;'}
     it { should contain 'proxy_set_header Proxy \'XYZZY\''}
   end
+  describe file('/etc/nginx/include.d/other_locations.conf') do
+    it { should exist }
+    it { should contain 'location ~* ^/external($|/.*$) {'}
+    it { should contain 'proxy_pass http://some-upstream;'}
+  end
 end
 
+describe 'Nginx upstreams' do
+  describe file('/etc/nginx/upstreams.d/') do
+    it { should be_directory }
+  end
+  describe file('/etc/nginx/upstreams.d/fragments_upstreams.conf') do
+    it { should exist }
+    it { should contain 'upstream some-upstream {'}
+    it { should contain 'server 127.0.0.1:1337'}
+    it { should contain 'upstream example.com:8080 {'}
+    it { should contain 'server example.com:8080'}
+  end
+  describe file('/etc/nginx/upstreams.d/other_upstreams.conf') do
+    it { should exist }
+    its(:content)  { is_expected.not_to include('upstream some-upstream {') }
+    its(:content)  { is_expected.not_to include('server localhost')}
+  end
+end
 
 describe 'NGinx http content' do
 
- describe command "curl -s -L http://127.0.0.1:1080" do 
+ describe command "curl -s -L http://127.0.0.1:1080" do
         its(:exit_status) { should eq 0 }
         its(:stdout) { should match "Hello test1" }
       end
